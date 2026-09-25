77bf4442a58d11e93749e69281ffe77a3a0b990f
KEI <332134846+MA-IOI-VE@users.noreply.github.com>
2026-09-25 17:51:34 +0900

    a

--- script.js
+++ script.js
@@ -87,9 +87,9 @@ const testRound2Button =
   document.getElementById("testRound2Button");
 
 testRound2Button.addEventListener("click", () => {
-results.love = [...members.slice(0, 0)];
-results.like = [...members.slice(0, 10)];
-results.normal = [...members.slice(10, 75)];
+results.love = [...members.slice(0, 10)];
+results.like = [...members.slice(10, 6)];
+results.normal = [...members.slice(6, 75)];
 
 console.log("テストNORMAL人数", results.normal.length);
 
